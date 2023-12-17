2fe27eeb9bb732d03f2d0af3dba63390095b3813
sarebok <bortonae@hotmail.com>
2023-12-17 17:51:13 -0300

    first commit gary

--- src/views/AgregarReceta.jsx
+++ src/views/AgregarReceta.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const AgregarReceta = () => {
-    return (
-        <>
-            <div>
-                <h1 className="titulos-global">AGREGA TU RECETA</h1>
-            </div>
-            <div className="home-container"></div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <h1 className="titulos-global">AGREGA TU RECETA prueba 1</h1>
+      </div>
+      <div className="home-container"></div>
+    </>
+  );
 };
 
 export default AgregarReceta;
