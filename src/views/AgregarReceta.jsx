1d6709fb00244c6e8078a0bc552544f0fa68e38c
Paola Gonzalez <paolagonzalez.contacto@gmail.com>
2023-12-18 03:36:05 -0300

    detallespao

--- src/views/AgregarReceta.jsx
+++ src/views/AgregarReceta.jsx
@@ -1,44 +1,44 @@
 import React, { useState } from "react";
 
 const AgregarReceta = () => {
-  const [recipe, setRecipe] = useState({
-    name: "",
-    ingredients: "",
-    image: "",
-    video: "",
-  });
+    const [recipe, setRecipe] = useState({
+        name: "",
+        ingredients: "",
+        image: "",
+        video: "",
+    });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setRecipe({ ...recipe, [name]: value });
-  };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setRecipe({ ...recipe, [name]: value });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aquí puedes manejar la lógica para agregar la receta
-    console.log("Receta enviada:", recipe);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Aquí puedes manejar la lógica para agregar la receta
+        console.log("Receta enviada:", recipe);
+    };
 
-  return (
-    <div className="back-form">
-      <h1 className="titulo-receta">agrega tu receta!</h1>
-      <form className="recipe-form" onSubmit={handleSubmit}>
-        <label htmlFor="name">Nombre:</label>
-        <input type="text" id="name" name="name" value={recipe.name} onChange={handleChange} />
+    return (
+        <div className="back-form">
+            <form className="recipe-form" onSubmit={handleSubmit}>
+                <h1 className="titulo-receta">Agrega Tu Receta</h1>
+                <label htmlFor="name">Nombre:</label>
+                <input type="text" id="name" name="name" value={recipe.name} onChange={handleChange} />
 
-        <label htmlFor="ingredients">Ingredientes:</label>
-        <textarea id="ingredients" name="ingredients" value={recipe.ingredients} onChange={handleChange} />
+                <label htmlFor="ingredients">Ingredientes:</label>
+                <textarea id="ingredients" name="ingredients" value={recipe.ingredients} onChange={handleChange} />
 
-        <label htmlFor="image">Imagen URL:</label>
-        <input type="text" id="image" name="image" value={recipe.image} onChange={handleChange} />
+                <label htmlFor="image">Imagen URL:</label>
+                <input type="text" id="image" name="image" value={recipe.image} onChange={handleChange} />
 
-        <label htmlFor="video">Video URL:</label>
-        <input type="text" id="video" name="video" value={recipe.video} onChange={handleChange} />
+                <label htmlFor="video">Video URL:</label>
+                <input type="text" id="video" name="video" value={recipe.video} onChange={handleChange} />
 
-        <button type="submit">Agregar Receta</button>
-      </form>
-    </div>
-  );
+                <button type="submit">Agregar Receta</button>
+            </form>
+        </div>
+    );
 };
 
 export default AgregarReceta;
--- src/views/MisRecetas.jsx
+++ src/views/MisRecetas.jsx
@@ -1,48 +1,48 @@
-import React from "react";
-
 const MisRecetas = () => {
-	const recipes = [
-		{
-			name: "Receta 1",
-			ingredients: "Ingrediente 1, Ingrediente 2",
-			type: "Tipo A",
-		},
-		{
-			name: "Receta 2",
-			ingredients: "Ingrediente 3, Ingrediente 4",
-			type: "Tipo B",
-		},
-		// ... otras recetas
-	];
-	return (
-		<>
-			<div className="back-form">
-				<h1 className="titulo-receta">Tus recetas</h1>
-				<div className="recipe-table">
-					<table>
-						<thead>
-							<tr>
-								<th>Número</th>
-								<th>Nombre</th>
-								<th>Ingredientes</th>
-								<th>Tipo</th>
-							</tr>
-						</thead>
-						<tbody>
-							{recipes.map((recipe, index) => (
-								<tr key={index}>
-									<td>{index + 1}</td>
-									<td>{recipe.name}</td>
-									<td>{recipe.ingredients}</td>
-									<td>{recipe.type}</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</>
-	);
+    const recipes = [
+        {
+            name: "Receta 1",
+            ingredients: "Ingrediente 1, Ingrediente 2",
+            type: "Tipo A",
+        },
+        {
+            name: "Receta 2",
+            ingredients: "Ingrediente 3, Ingrediente 4",
+            type: "Tipo B",
+        },
+        // ... otras recetas
+    ];
+    return (
+        <>
+            <div className="back-form-2">
+                <div className="seccion-receta-2">
+                    <h1 className="titulo-receta">Tus Recetas</h1>
+                    <div className="recipe-table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Número</th>
+                                    <th>Nombre</th>
+                                    <th>Ingredientes</th>
+                                    <th>Tipo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {recipes.map((recipe, index) => (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{recipe.name}</td>
+                                        <td>{recipe.ingredients}</td>
+                                        <td>{recipe.type}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default MisRecetas;
