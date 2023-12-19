541868290104e0444c2d7a309ebd93e2da21ff1d
jose medina <chesibb@gmail.com>
2023-12-19 20:05:46 -0300

    api y demases

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,56 +4,60 @@ import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import IosShareIcon from "@mui/icons-material/IosShare";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 
-const Card = () => {
-    const navigate = useNavigate();
-    const handleClick = (nombre) => {
-        const route = `/home/${nombre}`;
-        navigate(route);
-    };
+const Card = ({ receta }) => {
+	const navigate = useNavigate();
 
-    return (
-        <div className="contenedor-card">
-            <div>
-                <img className="imagen-card" src="../img/pexels-1.jpg" alt="imagenreceta"></img>
+	const handleClick = (nombre) => {
+		const route = `/home/${nombre}`;
+		navigate(route);
+	};
 
-                <div className="caja">
-                    <h4 className="titulo-caja">NOMBRE RECETA</h4>
-                    <NavLink to="/home/:nombre">
-                        <button className="btn-caja">Ver más</button>
-                    </NavLink>
-                </div>
-            </div>
-            <div className="iconos">
-                <button className="iconos-card">
-                    <BookmarkBorderIcon
-                        sx={{
-                            fontSize: 30,
-                            color: "#ffe8d6",
-                            margin: "0 1rem",
-                        }}
-                    />
-                </button>
-                <button className="iconos-card">
-                    <IosShareIcon
-                        sx={{
-                            fontSize: 30,
-                            color: "#ffe8d6",
-                            margin: "0 1rem",
-                        }}
-                    />
-                </button>
-                <button className="iconos-card">
-                    <ChatBubbleOutlineIcon
-                        sx={{
-                            fontSize: 28,
-                            color: "#ffe8d6",
-                            margin: "0 1rem",
-                        }}
-                    />
-                </button>
-            </div>
-        </div>
-    );
+	return (
+		<div className="contenedor-card">
+			<div>
+				<img
+					className="imagen-card"
+					src={receta.imagen}
+					alt="imagenreceta"
+				></img>
+				<div className="caja">
+					<h4 className="titulo-caja">{receta.nombre}</h4>
+					<NavLink to={`/home/${receta.nombre}`}>
+						<button className="btn-caja">Ver más</button>
+					</NavLink>
+				</div>
+			</div>
+			<div className="iconos">
+				<button className="iconos-card">
+					<BookmarkBorderIcon
+						sx={{
+							fontSize: 30,
+							color: "#ffe8d6",
+							margin: "0 1rem",
+						}}
+					/>
+				</button>
+				<button className="iconos-card">
+					<IosShareIcon
+						sx={{
+							fontSize: 30,
+							color: "#ffe8d6",
+							margin: "0 1rem",
+						}}
+					/>
+				</button>
+				<button className="iconos-card">
+					<ChatBubbleOutlineIcon
+						sx={{
+							fontSize: 28,
+							color: "#ffe8d6",
+							margin: "0 1rem",
+						}}
+					/>
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
--- src/context/RecetaContext.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RecetaContext = () => {
-    return <div>RecetaContext</div>;
-};
-
-export default RecetaContext;
