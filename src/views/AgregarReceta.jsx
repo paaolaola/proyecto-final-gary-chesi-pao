33998715f640c0d0463ebc64701cbfdc884e36c7
jose medina <chesibb@gmail.com>
2023-12-19 23:30:17 -0300

    form and weaitas

--- src/views/AgregarReceta.jsx
+++ src/views/AgregarReceta.jsx
@@ -1,48 +1,111 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import { GlobalContext } from "../context/GlobalProvider";
 
 const AgregarReceta = () => {
-    const [recipe, setRecipe] = useState({
-        name: "",
-        ingredients: "",
-        image: "",
-        video: "",
-    });
+	const { recetas, agregarReceta } = useContext(GlobalContext);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setRecipe({ ...recipe, [name]: value });
-    };
+	const [recipe, setRecipe] = useState({
+		nombre: "",
+		tipo: "",
+		ingredientes: "",
+		imagen: "",
+		video_youtube: "",
+		categoria: "",
+		descripcion: "",
+		preparacion: "",
+	});
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Aquí puedes manejar la lógica para agregar la receta
-        console.log("Receta enviada:", recipe);
-    };
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setRecipe({ ...recipe, [name]: value });
+	};
 
-    return (
-        <div className="back-form">
-            <Link to="/">
-                <button className="btn-global">Volver al home</button>
-            </Link>
-            <form className="recipe-form" onSubmit={handleSubmit}>
-                <h1 className="titulo-receta">Agrega Tu Receta</h1>
-                <label htmlFor="name">Nombre:</label>
-                <input type="text" id="name" name="name" value={recipe.name} onChange={handleChange} />
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		agregarReceta(recipe);
+		console.log("Receta enviada:", recipe);
+	};
 
-                <label htmlFor="ingredients">Ingredientes:</label>
-                <textarea id="ingredients" name="ingredients" value={recipe.ingredients} onChange={handleChange} />
+	return (
+		<div className="back-form">
+			<Link to="/">
+				<button className="btn-global">Volver al home</button>
+			</Link>
+			<form className="recipe-form" onSubmit={handleSubmit}>
+				<h1 className="titulo-receta">Agrega Tu Receta</h1>
+				<label htmlFor="nombre">Nombre:</label>
+				<input
+					type="text"
+					id="nombre"
+					name="nombre"
+					value={recipe.nombre}
+					onChange={handleChange}
+				/>
+				<label htmlFor="tipo">Tipo:</label>
+				<input
+					type="text"
+					id="tipo"
+					name="tipo"
+					value={recipe.tipo}
+					onChange={handleChange}
+				/>
 
-                <label htmlFor="image">Imagen URL:</label>
-                <input type="text" id="image" name="image" value={recipe.image} onChange={handleChange} />
+				<label htmlFor="ingredientes">Ingredientes:</label>
+				<textarea
+					id="ingredientes"
+					name="ingredientes"
+					value={recipe.ingredientes}
+					onChange={handleChange}
+				/>
 
-                <label htmlFor="video">Video URL:</label>
-                <input type="text" id="video" name="video" value={recipe.video} onChange={handleChange} />
+				<label htmlFor="imagen">Imagen URL:</label>
+				<input
+					type="text"
+					id="imagen"
+					name="imagen"
+					value={recipe.imagen}
+					onChange={handleChange}
+				/>
 
-                <button type="submit">Agregar Receta</button>
-            </form>
-        </div>
-    );
+				<label htmlFor="video_youtube">Video URL:</label>
+				<input
+					type="text"
+					id="video_youtube"
+					name="video_youtube"
+					value={recipe.video_youtube}
+					onChange={handleChange}
+				/>
+
+				<label htmlFor="categoria">Categoría:</label>
+				<input
+					type="text"
+					id="categoria"
+					name="categoria"
+					value={recipe.categoria}
+					onChange={handleChange}
+				/>
+
+				<label htmlFor="descripcion">Descripción:</label>
+				<textarea
+					id="descripcion"
+					name="descripcion"
+					value={recipe.descripcion}
+					onChange={handleChange}
+				/>
+
+				<label htmlFor="preparacion">Preparación:</label>
+				<textarea
+					id="preparacion"
+					name="preparacion"
+					value={recipe.preparacion}
+					onChange={handleChange}
+				/>
+
+				<button type="submit">Agregar Receta</button>
+			</form>
+		</div>
+	);
 };
 
 export default AgregarReceta;
