acf51168cc7fc19aec0bd0422668ac96d4daeb7c
sarebok <bortonae@hotmail.com>
2023-12-20 07:54:21 -0300

    favorite onclick selection implemented

--- src/context/GlobalProvider.jsx
+++ src/context/GlobalProvider.jsx
@@ -3,15 +3,12 @@ import { createContext, useState } from "react";
 export const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
-	const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
+  const [favorites, setFavorites] = useState([]);
 
-	const state = { data, setData };
+  const state = { data, setData, favorites, setFavorites };
 
-	return (
-		<GlobalContext.Provider value={state}>
-			{children}
-		</GlobalContext.Provider>
-	);
+  return <GlobalContext.Provider value={state}>{children}</GlobalContext.Provider>;
 };
 
 export default GlobalProvider;
