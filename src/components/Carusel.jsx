11a1a7276719af494f1650d82023f8208353e990
Paola Gonzalez <paolagonzalez.contacto@gmail.com>
2023-12-20 20:59:53 -0300

    csspao

--- src/components/Carusel.jsx
+++ src/components/Carusel.jsx
@@ -1,22 +1,22 @@
 import Carousel from "react-bootstrap/Carousel";
 
 function Carusel() {
-	return (
-		<Carousel className="carousel-header">
-			<Carousel.Item>
-				<img src="img/captura21-3.jpg" alt="" />
-				<Carousel.Caption></Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img src="img/images.jpg" alt="" />
-				<Carousel.Caption></Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img src="img/Menus-cocina-vital-banner.jpg" alt="" />
-				<Carousel.Caption></Carousel.Caption>
-			</Carousel.Item>
-		</Carousel>
-	);
+    return (
+        <Carousel className="carousel-header">
+            <Carousel.Item>
+                <img src="img/pexels-10.jpg" alt="" />
+                <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src="img/pexels-12.jpg" alt="" />
+                <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src="img/pexels-11.jpg" alt="" />
+                <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
 }
 
 export default Carusel;
