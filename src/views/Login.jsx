2864609ed0c6b8fbbdb166975dc6d5712f6e391c
jose medina <chesibb@gmail.com>
2023-12-21 03:00:10 -0300

    login toast

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,65 +1,81 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [user, setUser] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+	const [user, setUser] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(false);
 
-  //recordar que este login es estático.
-  const loginData = {
-    user: "admin",
-    password: "admin123",
-  };
+	//recordar que este login es estático.
+	const loginData = {
+		user: "admin",
+		password: "admin123",
+	};
 
-  const login = (e) => {
-    e.preventDefault();
+	const login = (e) => {
+		e.preventDefault();
 
-    // 1- Pedir el archivo JSON
-    // 2- Buscar el usuario
-    // 3- Revisar si la contraseña coincide para ese usuario
-    // 4.1- Si esta OK -> Se hace el login
-    // 4.2- Si no, manejamos el error
+		// 1- Pedir el archivo JSON
+		// 2- Buscar el usuario
+		// 3- Revisar si la contraseña coincide para ese usuario
+		// 4.1- Si esta OK -> Se hace el login
+		// 4.2- Si no, manejamos el error
 
-    if (user === loginData.user && password === loginData.password) {
-      sessionStorage.setItem("token", "test_gary");
-      console.log("localstorageasdasd", localStorage);
-      navigate("/");
-    } else {
-      setError(true);
-    }
-  };
+		if (user === loginData.user && password === loginData.password) {
+			sessionStorage.setItem("token", "test_gary");
+			console.log("localstorageasdasd", localStorage);
+			toast.success("Inicio de sesión exitoso");
+			navigate("/");
+		} else {
+			toast.error("Error en el inicio de sesión");
+			setError(true);
+		}
+	};
 
-  return (
-    <>
-      <div>
-        <h1 className="titulos-global">INGRESA A RECETAPP</h1>
-      </div>
+	return (
+		<>
+			<div>
+				<h1 className="titulos-global">INGRESA A RECETAPP</h1>
+			</div>
 
-      <div className="contenedor-login">
-        <div className="form">
-          <form onSubmit={(e) => login(e)}>
-            <div className="receta-form">
-              <input type="text " id="" placeholder="Usuario o correo electrónico" onChange={(e) => setUser(e.target.value)}></input>
-            </div>
-            <div className="receta-form">
-              <input type="password" id="" placeholder="******" onChange={(e) => setPassword(e.target.value)}></input>
-            </div>
-            <button className="btn-global" type="submit">
-              Ingresar
-            </button>
-            {error && <small>* Usuario o contraseña incorrecto(s)</small>}
-          </form>
-          <h5>
-            No tienes una cuenta? <Link to="/registro">Regístrate.</Link>
-          </h5>
-        </div>
-      </div>
-    </>
-  );
+			<div className="contenedor-login">
+				<div className="form">
+					<form onSubmit={(e) => login(e)}>
+						<div className="receta-form">
+							<input
+								type="text "
+								id=""
+								placeholder="Usuario o correo electrónico"
+								onChange={(e) => setUser(e.target.value)}
+							></input>
+						</div>
+						<div className="receta-form">
+							<input
+								type="password"
+								id=""
+								placeholder="******"
+								onChange={(e) => setPassword(e.target.value)}
+							></input>
+						</div>
+						<button className="btn-global" type="submit">
+							Ingresar
+						</button>
+						{error && (
+							<small>* Usuario o contraseña incorrecto(s)</small>
+						)}
+					</form>
+					<h5>
+						No tienes una cuenta?{" "}
+						<Link to="/registro">Regístrate.</Link>
+					</h5>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Login;
