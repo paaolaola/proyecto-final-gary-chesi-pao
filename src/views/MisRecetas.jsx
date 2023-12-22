39e9a8f761d6339a7c96ad25e2eb3e2fb9ca4707
Paola Gonzalez <paolagonzalez.contacto@gmail.com>
2023-12-21 23:56:30 -0300

    cambios misrecetas

--- src/views/MisRecetas.jsx
+++ src/views/MisRecetas.jsx
@@ -14,30 +14,34 @@ const MisRecetas = () => {
                 <div className="seccion-receta-2">
                     <h1 className="titulo-receta">Tus Recetas</h1>
 
-                    <Table className="recipe-table" striped bordered hover responsive>
-                        <thead className="thead-table">
-                            <tr>
-                                <th>Número</th>
-                                <th>Nombre</th>
-                                <th>Ingredientes</th>
-                                <th>Tipo</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {recetas.map((receta, index) => (
-                                <tr key={index}>
-                                    <td>{index + 1}</td>
-                                    <td>
-                                        <Link to={`/home/${receta.nombre}`} onClick={() => navigate(`/receta/${receta.nombre}`)}>
-                                            {receta.nombre}
-                                        </Link>
-                                    </td>
-                                    <td>{receta.ingredientes}</td>
-                                    <td>{receta.tipo}</td>
+                    {recetas.length === 0 ? (
+                        <p className="texto-condic">Aún no has creado recetas.</p>
+                    ) : (
+                        <Table className="recipe-table" striped bordered hover responsive>
+                            <thead className="thead-table">
+                                <tr>
+                                    <th>Número</th>
+                                    <th>Nombre</th>
+                                    <th>Ingredientes</th>
+                                    <th>Tipo</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </Table>
+                            </thead>
+                            <tbody>
+                                {recetas.map((receta, index) => (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>
+                                            <Link to={`/home/${receta.nombre}`} onClick={() => navigate(`/receta/${receta.nombre}`)}>
+                                                {receta.nombre}
+                                            </Link>
+                                        </td>
+                                        <td>{receta.ingredientes}</td>
+                                        <td>{receta.tipo}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    )}
                 </div>
             </div>
         </>
