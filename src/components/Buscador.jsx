bf7efdb2fc63dd4627ed4188b8828326d8a55b48
jose medina <chesibb@gmail.com>
2023-12-22 01:30:15 -0300

    login user

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -4,27 +4,34 @@ import { GlobalContext } from "../context/GlobalProvider";
 import OrdenarRecetas from "./OrdenarRecetas";
 
 const Buscador = () => {
-  const { data, setTextoBusqueda } = useContext(GlobalContext);
+	const { data, setTextoBusqueda, currentUser } = useContext(GlobalContext);
 
-  const handleSearchChange = (e) => {
-    setTextoBusqueda(e.target.value);
-  };
+	const handleSearchChange = (e) => {
+		setTextoBusqueda(e.target.value);
+	};
 
-  return (
-    <>
-      <div className="contenedor-busq">
-        <form className="form-busq">
-          <input type="search" placeholder="BUSCA RECETAS O INGREDIENTES" onChange={handleSearchChange}></input>
-          <button className="btn-busq">Buscar</button>
-        </form>
-        <div>
-          <NavLink to="/perfil">
-            <button className="btn-busq-perfil "></button>
-          </NavLink>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="contenedor-busq">
+				<form className="form-busq">
+					<input
+						type="search"
+						placeholder="BUSCA RECETAS O INGREDIENTES"
+						onChange={handleSearchChange}
+					></input>
+					<button className="btn-busq">Buscar</button>
+				</form>
+				<div className="user-div">
+					<NavLink to="/perfil">
+						<p className="user-text">
+							Bienvenido,{" "}
+							{currentUser ? currentUser.user : "Invitado"}
+						</p>
+					</NavLink>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Buscador;
